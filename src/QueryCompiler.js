12eb84ef5bd5d3bd5824e76dd5cbd9064711e5e8
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-02-07 21:15:08 +0200

    Add compileSelection, compileOrderBy and compileProjection to QueryCompiler

--- src/QueryCompiler.js
+++ src/QueryCompiler.js
@@ -5,21 +5,372 @@ import {QueryCompiler} from 'naomi';
 class MySqlQueryCompiler extends QueryCompiler {
 
   /**
-   * Builds and returns a key based on the supplied AST.
+   * Compiles and returns a parameterized SQL KEY expression, based on the supplied AST.
    * @param {Array} ast abstract syntax tree.
    * @return {Object}
    */
-  compileKey(ast: Array) {
+  compileKey(ast: Array): Object {
+    if (ast[0] !== 'KEY') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "KEY", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const sql = `\`${ast[1]}\``;
+    const params = [];
+
+    return {sql, params};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL VALUE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileValue(ast: Array): Object {
+    if (ast[0] !== 'VALUE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "VALUE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const sql = '?';
+    const params = [ast[1]];
+
+    return {params, sql};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL VALUE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileValues(ast: Array): Object {
+    if (ast[0] !== 'VALUES') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "VALUES", received ${ast[0]}`, 'QueryCompileException');
+    }
+
     const sql = [];
     const params = [];
 
-    if (ast[0] !== 'KEY') {
-      throw new CustomError('Invalid key AST', 'QueryCompileException');
+    _.tail(ast).forEach((e) => {
+      sql.push('?');
+      params.push(e);
+    });
+
+    return {params, sql: '(' + sql.join(', ') + ')'};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL EQ expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileEq(ast: Array): Object {
+    if (ast[0] !== 'EQ') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "EQ", received ${ast[0]}`, 'QueryCompileException');
     }
 
-    sql.push(`\`${ast[1]}\``);
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
 
-    return {params, sql: sql.join('')};
+    const sql = [key.sql];
+    let params = key.params;
+
+    if (value.params[0] === null) {
+      sql.push('IS NULL');
+    } else {
+      sql.push('=', value.sql);
+      params = params.concat(value.params);
+    }
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL EQ expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileNe(ast: Array): Object {
+    if (ast[0] !== 'NE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "NE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    if (value.params[0] === null) {
+      sql.push('IS NOT NULL');
+    } else {
+      sql.push('!=', value.sql);
+      params = params.concat(value.params);
+    }
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL GT expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileGt(ast: Array): Object {
+    if (ast[0] !== 'GT') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "GT", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('>', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL GTE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileGte(ast: Array): Object {
+    if (ast[0] !== 'GTE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "GTE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('>=', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL LT expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileLt(ast: Array): Object {
+    if (ast[0] !== 'LT') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "LT", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('<', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL LTE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileLte(ast: Array): Object {
+    if (ast[0] !== 'LTE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "LTE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('<=', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL IN expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileIn(ast: Array): Object {
+    if (ast[0] !== 'IN') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "IN", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const values = this.compileValues(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('IN', values.sql);
+    params = params.concat(values.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL NIN expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileNin(ast: Array): Object {
+    if (ast[0] !== 'NIN') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "NIN", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const values = this.compileValues(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('NOT IN', values.sql);
+    params = params.concat(values.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL LIKE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileLike(ast: Array): Object {
+    if (ast[0] !== 'LIKE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "LIKE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('LIKE', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL NLIKE expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileNlike(ast: Array): Object {
+    if (ast[0] !== 'NLIKE') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "NLIKE", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const key = this.compileKey(ast[1]);
+    const value = this.compileValue(ast[2]);
+
+    const sql = [key.sql];
+    let params = key.params;
+
+    sql.push('NOT LIKE', value.sql);
+    params = params.concat(value.params);
+
+    return {params, sql: sql.join(' ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL AND expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileAnd(ast: Array): Object {
+    if (ast[0] !== 'AND') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "AND", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const sql = [];
+    let params = [];
+
+    _.tail(ast).forEach((e) => {
+      const expr = this.compileSelection(['SELECTION', e]);
+      sql.push('(' + expr.sql + ')');
+      params = params.concat(expr.params);
+    });
+
+    return {params, sql: sql.join(' AND ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL OR expression, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileOr(ast: Array): Object {
+    if (ast[0] !== 'OR') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "OR", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    const sql = [];
+    let params = [];
+
+    _.tail(ast).forEach((e) => {
+      const expr = this.compileSelection(['SELECTION', e]);
+      sql.push('(' + expr.sql + ')');
+      params = params.concat(expr.params);
+    });
+
+    return {params, sql: sql.join(' OR ')};
+  }
+
+  /**
+   * Compiles and returns a parameterized SQL SELECTION clause, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileSelection(ast: Array): Object {
+    if (ast[0] !== 'SELECTION') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "SELECTION", received ${ast[0]}`, 'QueryCompileException');
+    }
+
+    if (_.isNil(ast[1])) {
+      return {sql: '', params: []};
+    }
+
+    switch (ast[1][0]) {
+    case 'EQ':
+      return this.compileEq(ast[1]);
+    case 'NE':
+      return this.compileNe(ast[1]);
+    case 'GT':
+      return this.compileGt(ast[1]);
+    case 'GTE':
+      return this.compileGte(ast[1]);
+    case 'LT':
+      return this.compileLt(ast[1]);
+    case 'LTE':
+      return this.compileLte(ast[1]);
+    case 'IN':
+      return this.compileIn(ast[1]);
+    case 'NIN':
+      return this.compileNin(ast[1]);
+    case 'LIKE':
+      return this.compileLike(ast[1]);
+    case 'NLIKE':
+      return this.compileNlike(ast[1]);
+    case 'AND':
+      return this.compileAnd(ast[1]);
+    case 'OR':
+      return this.compileOr(ast[1]);
+    default:
+      throw new CustomError(`Invalid abstract syntax tree; unknown identifier: ${ast[0]}`, 'QueryCompileException');
+    }
   }
 
   /**
@@ -27,89 +378,82 @@ class MySqlQueryCompiler extends QueryCompiler {
    * @param {Array} ast abstract syntax tree.
    * @return {Object}
    */
-  compileOrderBy(ast: Array) {
-    let sql = [];
+  compileOrderBy(ast: Array): Object {
+    const sql = [];
     let params = [];
 
-    switch (ast[0]) {
-    case 'ORDERBY':
-      sql.push('ORDER BY');
-      _.tail(ast).forEach((e) => {
-        const clause = this.compileOrderBy(e);
-        sql = sql.concat(clause.sql);
-        params = params.concat(clause.params);
-      });
-      break;
-    case 'DESC':
-      (() => {
-        const clause = this.compileKey(ast[1]);
-        sql = sql.concat(clause.sql);
-        params = params.concat(clause.params);
-      })();
-      sql.push('DESC');
-      break;
-    case 'ASC':
-      (() => {
-        const clause = this.compileKey(ast[1]);
-        sql = sql.concat(clause.sql);
-        params = params.concat(clause.params);
-      })();
-      sql.push('ASC');
-      break;
-    default:
-      throw new CustomError('Invalid orderby AST', 'QueryCompileException');
+    if (ast[0] !== 'ORDERBY') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "ORDERBY", received ${ast[0]}`, 'QueryCompileException');
     }
 
-    return {params, sql: sql.join('')};
+    if (!_.isNil(ast[1])) {
+      _.tail(ast).forEach((arr) => {
+        if (arr[0] === 'DESC') {
+          const key = this.compileKey(arr[1]);
+          sql.push(`${key.sql} DESC`);
+          params = params.concat(key.params);
+        } else if (arr[0] === 'ASC') {
+          const key = this.compileKey(arr[1]);
+          sql.push(`${key.sql} ASC`);
+          params = params.concat(key.params);
+        } else {
+          throw new CustomError(`Invalid abstract syntax tree; expected "ASC" or "DESC", received ${ast[0]}`, 'QueryCompileException');
+        }
+      });
+    }
+
+    return {params, sql: sql.join(', ')};
   }
 
-  // _compile(ast: Array): Object {
-  //   let sql = [];
-  //   let params = [];
+  /**
+   * Compiles and returns a parameterized SQL PROJECTION clause, based on the supplied AST.
+   * @param {Array} ast abstract syntax tree.
+   * @return {Object}
+   */
+  compileProjection(ast: Array): Object {
+    if (ast[0] !== 'PROJECTION') {
+      throw new CustomError(`Invalid abstract syntax tree; expected "PROJECTION", received ${ast[0]}`, 'QueryCompileException');
+    }
 
-  //   if (ast.length !== 0) {
-  //     switch (ast[0]) {
-  //     case 'KEY':
-  //       sql.push(this.escape(ast[1]));
-  //       break;
+    const included = [];
+    const excluded = [];
 
-  //     case 'VALUE':
-  //       sql.push('?');
-  //       params.push(ast[1]);
-  //       break;
+    if (!_.isNil(ast[1])) {
+      _.tail(ast).forEach((e) => {
+        switch (e[0]) {
+        case 'INCLUDE':
+          included.push(e[1]);
+          break;
+        case 'EXCLUDE':
+          excluded.push(e[1]);
+          break;
+        default:
+          throw new CustomError(`Invalid abstract syntax tree; expected "INCLUDE" or "EXCLUDE", received ${ast[0]}`, 'QueryCompileException');
+        }
+      });
+    }
 
-  //     case 'EQ': () => {
-  //       const a = this._compile(ast[1]);
-  //       const b = this._compile(ast[2]);
+    if (included.length === 0) {
+      included = this.properties.map((e) => {
+        return ['KEY', e];
+      });
+    }
 
-  //       sql = sql.concat(a.sql);
-  //       sql.push('=');
-  //       sql = sql.concat(b.sql);
+    if (excluded.length === 0) {
+      included = _.differenceWith(included, excluded, _.isEqual);
+    }
 
-  //       params = params.concat(a.params);
-  //       params = params.concat(b.params);
-  //     }();
-  //     break;
+    const sql = [];
+    let params = [];
 
-  //     case 'NE':
-  //       const a = this._compile(ast[1]);
-  //       const b = this._compile(ast[2]);
+    included.forEach((e) => {
+      const key = this.compileKey(e);
+      sql.push(key.sql);
+      params = params.concat(params);
+    });
 
-  //       sql = sql.concat(a.sql);
-  //       sql.push('!=');
-  //       sql = sql.concat(b.sql);
-
-  //       params = params.concat(a.params);
-  //       params = params.concat(b.params);
-  //       break;
-
-  //     default:
-  //       throw new Error('Unknown operator');
-  //     }
-  //   }
-
-  //   return {params, sql: sql.join(' ')};
-  // }
+    return {params, sql: sql.join(', ')};
+  }
 
   buildFind(ast: Array): Object {
     const sql = [];
--- test/querycompiler.js
+++ test/querycompiler.js
@@ -7,7 +7,7 @@ describe('QueryCompiler', function () {
   const builder = new QueryCompiler('employees');
 
   describe('#compileKey()', function () {
-    it('compiles AST', function () {
+    it('accepts valid AST', function () {
       const query = builder.compileKey(['KEY', 'a']);
 
       assert.strictEqual(query.sql, '`a`');
@@ -16,15 +16,186 @@ describe('QueryCompiler', function () {
   });
 
   describe('#compileOrderBy()', function () {
-    it('compiles AST', function () {
+    it('accepts valid AST', function () {
       const query = builder.compileOrderBy([
         'ORDERBY',
         ['ASC', ['KEY', 'foo']],
         ['DESC', ['KEY', 'bar']]
       ]);
 
-      assert.strictEqual(query.sql, 'ORDER BY `foo` ASC, `bar` DESC');
+      assert.strictEqual(query.sql, '`foo` ASC, `bar` DESC');
       assert.lengthOf(query.params, 0);
+    });
+  });
+
+  describe('#compileSelection()', function () {
+    it('accepts AST with nil arguments', function () {
+      const query = builder.compileSelection(['SELECTION', null]);
+
+      assert.strictEqual(query.sql, '');
+      assert.deepEqual(query.params, []);
+    });
+
+    it('accepts AST containing $eq', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['EQ', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` = ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $eq with null value', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['EQ', ['KEY', 'foo'], ['VALUE', null]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` IS NULL');
+      assert.deepEqual(query.params, []);
+    });
+
+    it('accepts AST containing $ne', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['NE', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` != ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $ne with null value', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['NE', ['KEY', 'foo'], ['VALUE', null]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` IS NOT NULL');
+      assert.deepEqual(query.params, []);
+    });
+
+    it('accepts AST containing $gt', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['GT', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` > ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $gte', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['GTE', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` >= ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $lt', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['LT', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` < ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $lte', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['LTE', ['KEY', 'foo'], ['VALUE', 123]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` <= ?');
+      assert.deepEqual(query.params, [123]);
+    });
+
+    it('accepts AST containing $in', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['IN', ['KEY', 'foo'], ['VALUES', 1, 2, 3]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` IN (?, ?, ?)');
+      assert.deepEqual(query.params, [1, 2, 3]);
+    });
+
+    it('accepts AST containing $nin', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['NIN', ['KEY', 'foo'], ['VALUES', 1, 2, 3]]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` NOT IN (?, ?, ?)');
+      assert.deepEqual(query.params, [1, 2, 3]);
+    });
+
+    it('accepts AST containing $like', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['LIKE', ['KEY', 'foo'], ['VALUE', '%str']]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` LIKE ?');
+      assert.deepEqual(query.params, ['%str']);
+    });
+
+    it('accepts AST containing $nlike', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['NLIKE', ['KEY', 'foo'], ['VALUE', '%str']]
+      ]);
+
+      assert.strictEqual(query.sql, '`foo` NOT LIKE ?');
+      assert.deepEqual(query.params, ['%str']);
+    });
+
+    it('accepts AST containing $and', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['AND',
+          ['EQ', ['KEY', 'foo'], ['VALUE', 123]],
+          ['NE', ['KEY', 'bar'], ['VALUE', 'str']]
+        ]
+      ]);
+
+      assert.strictEqual(query.sql, '(`foo` = ?) AND (`bar` != ?)');
+      assert.deepEqual(query.params, [123, 'str']);
+    });
+
+    it('accepts AST containing $or', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['OR',
+          ['EQ', ['KEY', 'foo'], ['VALUE', 123]],
+          ['NE', ['KEY', 'bar'], ['VALUE', 'str']]
+        ]
+      ]);
+
+      assert.strictEqual(query.sql, '(`foo` = ?) OR (`bar` != ?)');
+      assert.deepEqual(query.params, [123, 'str']);
+    });
+
+    it('accepts AST containing $and with nested $or', function () {
+      const query = builder.compileSelection([
+        'SELECTION',
+        ['AND',
+          ['OR',
+            ['EQ', ['KEY', 'foo'], ['VALUE', 1]],
+            ['EQ', ['KEY', 'foo'], ['VALUE', 2]]
+          ],
+          ['NE', ['KEY', 'bar'], ['VALUE', 'str']]
+        ]
+      ]);
+
+      assert.strictEqual(query.sql, '((`foo` = ?) OR (`foo` = ?)) AND (`bar` != ?)');
+      assert.deepEqual(query.params, [1, 2, 'str']);
     });
   });
 
